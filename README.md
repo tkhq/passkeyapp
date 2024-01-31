50b7d4a0f5899b7373066f9f850c8db2b6fe44ac
Arnaud Brousseau <rno@turnkey.io>
2024-01-31 17:14:47 -0600

    Add worker function to set the right MIME type

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Passkey app
 
 This repo contains a sample passkey app which creates/uses passkeys. For testing purposes only.
+
+## Running the app locally
+
+`npm run ios` will start the app using expo.
+
+## `http` folder
+
+In the HTTP folder you'll find a folder with what's hosted at passkeyapp.tkhqlabs.xyz. It contains a Cloudflare worker function to give apple-app-site-association the right MIME type.
+
+To run it locally: `npx wrangler pages dev http`
---
+++ http/_worker.js
@@ -0,0 +1,20 @@
+// Adds the proper MIME type to serve apple-app-site-association file
+export default {
+    async fetch(request, env) {
+
+        // Fetch the asset
+        const response = await env.ASSETS.fetch(request);
+        
+        // Response is immutable by default. Fix this.
+        const responseClone = new Response(response.body, response);
+
+        // Force application/json for apple-app-site-association file
+        const url = new URL(request.url);
+        if (url.pathname.endsWith('apple-app-site-association')) {
+            responseClone.headers.set("Content-Type", "application/json");
+        }
+
+        // Return our response!
+        return responseClone;
+    },
+}
